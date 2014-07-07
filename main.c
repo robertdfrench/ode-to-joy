89071aaec1e330b3751977b85991414688928818
Robert D. French <frenchrd@ornl.gov>
2014-07-06 20:52:41 -0400

    Adding grid size options

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <mpi.h>
 
 typedef struct stepsize_t {
 	double x;
@@ -13,6 +14,12 @@ typedef struct grid_t {
 	int len_x;
 	int len_y;
 } Grid;
+
+typedef struct grid_options_t {
+	size_t len_x;
+	size_t len_y;
+	size_t len_t;
+} GridOptions;
 
 #define grid_element(g,i,j) g.internal_storage[(i * g.len_x) + (j - 1)]
 
@@ -74,10 +81,6 @@ void apply_boundary_conditions(Grid g) {
 	for(j=0; j < g.len_y; j++) grid_element(g,g.len_x - 1,j) = 0;
 }
 
-#define LEN_X 500
-#define LEN_Y 500
-#define LEN_T 10
-
 void store_grid(Grid g) {
 	FILE* gridFile = fopen("output.otj_grid","w");
 	size_t num_cells = g.len_x * g.len_y;
@@ -88,25 +91,84 @@ void store_grid(Grid g) {
 	}
 }
 	
+typedef struct timing_measurement_t {
+	double beginning;
+	double end;
+	char* message;
+} TimingMeasurement;
+
+TimingMeasurement start_timer(char* message) {
+	TimingMeasurement tm;
+	tm.message = message;
+	tm.beginning = MPI_Wtime();
+	tm.end = tm.beginning;
+	return tm;
+}
+
+#define stop_timer(tm) printf("[Timer] %s: %f seconds\n", tm.message, MPI_Wtime() - tm.beginning); 
+
+GridOptions parse_grid_options(int argc, char** argv) {
+	GridOptions go;
+	// Set defaults
+	go.len_x = 500;
+	go.len_y = 500;
+	go.len_t = 10;
+
+	int i = 1;
+	while(i < argc - 1) {
+		if (strcmp(argv[i],"-x") == 0) {
+			go.len_x = atoi(argv[i+1]);
+			i += 2;
+		} else if (strcmp(argv[i],"-y") == 0) {
+			go.len_y = atoi(argv[i+1]);
+			i += 2;
+		} else if (strcmp(argv[i],"-t") == 0) {
+			go.len_t = atoi(argv[i+1]);
+			i += 2;
+		} else {
+			printf("I'm sorry, I don't recognize the %s option.\n", argv[i]);
+			printf("Options:\n");
+			printf("\t-xX : Sets width of grid to X in the x direction\n");
+			printf("\t-yY : Sets width of grid to Y in the y direction\n");
+			printf("\t-tT : Sets number of timesteps to T\n");
+			exit(1);
+		}
+	}
+	return go;
+}
+
+Stepsize stepsize_from_grid_options(GridOptions go) {
+	Stepsize h;
+	h.x = 1.0 / ((double) go.len_x);
+	h.y = 1.0 / ((double) go.len_y);
+	h.t = 1.0 / ((double) go.len_t);
+	return h;
+}
 
 int main(int argc, char** argv) {
-	Grid initial_conditions = generate_initial_conditions(LEN_X, LEN_Y);
-	Stepsize h;
-	h.x = 1.0 / ((double) LEN_X);
-	h.y = 1.0 / ((double) LEN_Y);
-	h.t = 1.0 / ((double) LEN_T);
+	MPI_Init(&argc, &argv);
+	GridOptions go = parse_grid_options(argc, argv);
 
-	Grid grids_by_timestep[LEN_T];
+	TimingMeasurement tm = start_timer("Initial Conditions");
+	Grid initial_conditions = generate_initial_conditions(go.len_x, go.len_y);
+	stop_timer(tm);
+
+	Stepsize h = stepsize_from_grid_options(go);
+	Grid grids_by_timestep[go.len_t];
 	grids_by_timestep[0] = initial_conditions;
 
+	tm = start_timer("Solve Problem");
 	int tau;
-	for(tau = 1; tau < LEN_T; tau++) {
-		grids_by_timestep[tau] = alloc_grid(LEN_X, LEN_Y);
+	for(tau = 1; tau < go.len_t; tau++) {
+		grids_by_timestep[tau] = alloc_grid(go.len_x, go.len_y);
 		apply_boundary_conditions(grids_by_timestep[tau]);
 		solve_interior(grids_by_timestep[tau], grids_by_timestep[tau - 1],h);
 	}
+	stop_timer(tm);
 
-	store_grid(grids_by_timestep[LEN_T - 1]);
+	tm = start_timer("Store Grid");
+	store_grid(grids_by_timestep[go.len_t - 1]);
+	stop_timer(tm);
 	
 	return 0;
 }
