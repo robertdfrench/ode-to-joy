72a052d981593e5b787f94a2cbb152ca99adc900
Robert D. French <frenchrd@ornl.gov>
2014-08-14 10:43:17 -0400

    Turn and face the strange ch-ch-changes

--- main.c
+++ main.c
@@ -22,6 +22,46 @@ OTJ_Grid generate_initial_conditions(int len_x, int len_y) {
 	return initial_conditions;
 }
 
+void populate_analytic_solution(OTJ_Grid g, int tau) {
+	int i,j;
+	for (i = 0; i < g.len_x; i++) {
+		for (j = 0; j < g.len_y; j++) {
+			OTJ_Grid_Element(g,i,j) = analytic_solution(g.hx * i, g.hy * j, g.ht * tau);
+		}
+	}
+}
+
+void calculate_error(OTJ_Grid error, OTJ_Grid analytic, OTJ_Grid numerical) {
+	int i,j;
+	for (i = 0; i < g.len_x; i++) {
+		for (j = 0; j < g.len_y; j++) {
+			OTJ_Grid_Element(error,i,j) = abs(OTJ_Grid_Element(analytic,i,j) - OTJ_Grid_Element(numerical,i,j));
+		}
+	}
+}
+
+double global_error(OTJ_Grid error) {
+	double max = 0.0;
+	int i,j;
+	for (i = 0; i < g.len_x; i++) {
+		for (j = 0; j < g.len_y; j++) {
+			double e = OTJ_Grid_Element(error,i,j);
+			max = (e > max) ? e : max;
+		}
+	}
+	return max;
+}
+
+double analytic_solution(double x, double y, double t) {
+	int infinity = 20;
+	int r;
+	double sum = 0.0;
+	for(r = 1; r < infinity; r++) {
+		sum += (1 - pow(-1,r)) * sin(0.5 * r * PI * x) * pow(E,-0.25 * PI * PI * (r * r + 1) * t);
+	}
+	return sin(0.5 * PI * y) * sum;
+}
+
 void swap_grids(OTJ_Grid a, OTJ_Grid b) {
 	double* temp = a.internal_storage;
 	a.internal_storage = b.internal_storage;
@@ -52,6 +92,16 @@ int main(int argc, char** argv) {
 	OTJ_Grid solution_grid = OTJ_Grid_Alloc(go.len_x, go.len_y);
 	retrieveDeviceGrid(solution_grid, current_grid);
 	OTJ_Timer_Stop(tm);
+
+	tm = OTJ_Timer_Start("Compare to analytic Solution");
+	OTJ_Grid analytic_grid = OTJ_Grid_Alloc(go.len_x, go.len_y);
+	populate_analytic_solution(analytic_grid);
+	// Overwrite analytic soln with error
+	calculate_error(analytic_grid, analytic_grid, solution_grid);
+	double gte = global_error(analytic_grid);
+	OTJ_Timer_Stop(tm);
+
+	printf("Global Truncation Error: %f\n",gte);
 
 	tm = OTJ_Timer_Start("Store Grid");
 	OTJ_Grid_Store(solution_grid);
