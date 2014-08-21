8f7fd9c596a3d2836134ce89171a8efe9c88ee25
Robert D. French <frenchrd@ornl.gov>
2014-08-21 16:29:34 -0400

    Add debugging, fix initial conditions

--- Makefile
+++ Makefile
@@ -1,22 +1,22 @@
 include make.inc
 otj.exe: main.c solve_interior.o grid.o stepsize.o grid_options.o timing_measurement.o
-	$(MPICC) -o otj.exe $(CUDA_LIBRARY_PATH) -lcudart solve_interior.o grid.o stepsize.o grid_options.o timing_measurement.o main.c
+	$(MPICC) $(DEBUG) -o otj.exe $(CUDA_LIBRARY_PATH) -lcudart solve_interior.o grid.o stepsize.o grid_options.o timing_measurement.o main.c
 	cp otj.exe $(DESTINATION)
 
 grid.o: grid.c grid.h
-	$(MPICC) -c grid.c
+	$(MPICC)  $(DEBUG) -c grid.c
 
 stepsize.o: stepsize.c stepsize.h grid_options.o
-	$(MPICC) -c stepsize.c
+	$(MPICC)  $(DEBUG) -c stepsize.c
 
 grid_options.o: grid_options.c grid_options.h
-	$(MPICC) -c grid_options.c
+	$(MPICC)  $(DEBUG) -c grid_options.c
 
 solve_interior.o: solve_interior.cu
-	$(NVCC) -c solve_interior.cu
+	$(NVCC) $(DEBUG)  -c solve_interior.cu
 
 timing_measurement.o: timing_measurement.c timing_measurement.h
-	$(MPICC) -c timing_measurement.c
+	$(MPICC) $(DEBUG)  -c timing_measurement.c
 
 clean:
 	rm -f *.o
--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 
 
 double analytic_solution(double x, double y, double t) {
-	int infinity = 20;
+	int infinity = 1;
 	int r;
 	double sum = 0.0;
 	for(r = 1; r < infinity; r++) {
@@ -27,7 +27,7 @@ OTJ_Grid generate_initial_conditions(int len_x, int len_y) {
 	double hx = 1.0 / ((double) len_x);
 	for (i = 0; i < len_x; i++) {
 		for (j = 0; j < len_y; j++) {
-			OTJ_Grid_Element(initial_conditions,i,j) = sin(i * hx);
+			OTJ_Grid_Element(initial_conditions,i,j) = sin(M_PI * i * hx);
 		}
 	}
 	return initial_conditions;
@@ -46,7 +46,10 @@ void calculate_error(OTJ_Grid error, OTJ_Grid analytic, OTJ_Grid numerical) {
 	int i,j;
 	for (i = 0; i < error.len_x; i++) {
 		for (j = 0; j < error.len_y; j++) {
-			OTJ_Grid_Element(error,i,j) = abs(OTJ_Grid_Element(analytic,i,j) - OTJ_Grid_Element(numerical,i,j));
+			double a = OTJ_Grid_Element(analytic,i,j);
+			double n = OTJ_Grid_Element(numerical,i,j);
+			
+			OTJ_Grid_Element(error,i,j) = abs(n - a);
 		}
 	}
 }
@@ -63,10 +66,21 @@ double global_error(OTJ_Grid error) {
 	return max;
 }
 
-void swap_grids(OTJ_Grid a, OTJ_Grid b) {
-	double* temp = a.internal_storage;
-	a.internal_storage = b.internal_storage;
-	b.internal_storage = temp;
+double total_error(OTJ_Grid error) {
+	double total_error = 0.0;
+	int i,j;
+	for (i = 0; i < error.len_x; i++) {
+		for (j = 0; j < error.len_y; j++) {
+			total_error += OTJ_Grid_Element(error,i,j);
+		}
+	}
+	return total_error;
+}
+
+void swap_grids(OTJ_Grid* a, OTJ_Grid* b) {
+	double* temp = a->internal_storage;
+	a->internal_storage = b->internal_storage;
+	b->internal_storage = temp;
 }
 
 int main(int argc, char** argv) {
@@ -83,10 +97,10 @@ int main(int argc, char** argv) {
 
 	OTJ_Grid current_grid = createDeviceGrid(initial_conditions);
 	OTJ_Grid previous_grid = createAndCopyDeviceGrid(initial_conditions);
-	swap_grids(current_grid, previous_grid);
+	swap_grids(&current_grid, &previous_grid);
 	int tau;
 	for(tau = 1; tau < go.len_t; tau++) {
-		swap_grids(current_grid, previous_grid);
+		swap_grids(&current_grid, &previous_grid);
 		apply_boundary_conditions(current_grid);
 		solve_interior(current_grid, previous_grid,h);
 	}
@@ -100,9 +114,11 @@ int main(int argc, char** argv) {
 	// Overwrite analytic soln with error
 	calculate_error(analytic_grid, analytic_grid, solution_grid);
 	double gte = global_error(analytic_grid);
+	double tote = total_error(analytic_grid);
 	OTJ_Timer_Stop(tm);
 
 	printf("Global Truncation Error: %f\n",gte);
+	printf("Total Error: %f\n",tote);
 
 	tm = OTJ_Timer_Start("Store Grid");
 	OTJ_Grid_Store(solution_grid);
