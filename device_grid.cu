61ac0d75ce211277e814edfebaffbf44320d49e9
Robert D. French <frenchrd@ornl.gov>
2014-09-11 17:06:23 -0400

    Split up host and device grid

--- device_grid.cu
+++ device_grid.cu
@@ -1,23 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "grid.h"
-void OTJ_Grid_Store(OTJ_Grid g) {
-	FILE* gridFile = fopen("output.otj_grid","w");
-	size_t num_cells = g.len_x * g.len_y;
-	size_t cell_size = sizeof(double);
-	size_t elements_written = fwrite(g.internal_storage, cell_size, num_cells, gridFile);
-	if (elements_written < num_cells) {
-		printf("An error occurred while saving the grid.\n");
-	}
-}
-
-OTJ_Grid OTJ_Grid_Alloc(int len_x, int len_y) {
-	OTJ_Grid g;
-	g.len_x = len_x;
-	g.len_y = len_y;
-	g.internal_storage = (double*)malloc(sizeof(double) * len_x * len_y);
-	return g;
-}
+#include <cuda.h>
 
 extern "C" OTJ_Grid createDeviceGrid(OTJ_Grid host_grid) {
 	OTJ_Grid device_grid = host_grid;
