9ee75101d7997cdfe5b9e6112199961ca894a2bc
robertdfrench <frenchrd@ornl.gov>
2014-09-22 19:33:56 -0400

    Olsen-Ketle suggests h.x = 1 / (len_x +1)

--- stepsize.c
+++ stepsize.c
@@ -2,8 +2,10 @@
 
 Stepsize stepsize_from_grid_options(GridOptions go) {
 	Stepsize h;
-	h.x = 1.0 / ((double) go.len_x);
-	h.y = 1.0 / ((double) go.len_y);
+	// Add one to the denominator because both
+	// spatial boundaries are part of the problem
+	h.x = 1.0 / ((double) go.len_x + 1);
+	h.y = 1.0 / ((double) go.len_y + 1);
 	h.t = 1.0 / ((double) go.len_t);
 	return h;
 }
